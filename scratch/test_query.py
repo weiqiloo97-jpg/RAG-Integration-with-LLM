4f819598fe360295ec900dab7481d9982505478f
Loo Weiqi Jae <weiqiloo97@gmail.com>
2026-07-17 00:21:51 +0800

    minor change

--- scratch/test_query.py
+++
@@ -1,6 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="./my_local_database")
-collection = client.get_collection("langchain")
-print("Collection metadata:", collection.metadata)
-print("Collection embedding function:", collection._embedding_function)
